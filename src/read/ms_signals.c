992ec684d0b73a8178080321a137c0a0f3dedf8b
atan94 <amostan94@gmail.com>
2026-03-02 12:56:42 +0800

    refactor: consolidate signal action setup in set_signals function

--- src/read/ms_signals.c
+++ src/read/ms_signals.c
@@ -6,7 +6,7 @@
 /*   By: amtan <amtan@student.42singapore.sg>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/21 05:56:54 by yunguo            #+#    #+#             */
-/*   Updated: 2026/03/02 10:59:14 by amtan            ###   ########.fr       */
+/*   Updated: 2026/03/02 12:55:52 by amtan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,18 +21,22 @@ static void	handle_sigint(int sig)
 	write(1, "^C", 2);
 }
 
-void	set_signals(void)
+static int	ms_sigaction(int sig, void (*handler)(int))
 {
 	struct sigaction	sa;
 
-	sa.sa_handler = handle_sigint;
+	sa.sa_handler = handler;
 	sigemptyset(&sa.sa_mask);
 	sa.sa_flags = 0;
-	sigaction(SIGINT, &sa, NULL);
-	sa.sa_handler = SIG_IGN;
-	sigemptyset(&sa.sa_mask);
-	sa.sa_flags = 0;
-	sigaction(SIGQUIT, &sa, NULL);
+	return (sigaction(sig, &sa, NULL));
+}
+
+void	set_signals(void)
+{
+	if (ms_sigaction(SIGINT, handle_sigint) == -1)
+		return (perror("sigaction(SIGINT)"));
+	if (ms_sigaction(SIGQUIT, SIG_IGN) == -1)
+		return (perror("sigaction(SIGQUIT)"));
 }
 
 char	*read_multiline_sigint(char *rslt)
