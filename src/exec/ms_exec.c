06ac9c0a02626b905b6473c64b7217e50eee7b58
atan94 <amostan94@gmail.com>
2026-03-06 13:13:58 +0800

    feat: refactor command execution flow to handle logical operators in AST

--- src/exec/ms_exec.c
+++ src/exec/ms_exec.c
@@ -6,7 +6,7 @@
 /*   By: amtan <amtan@student.42singapore.sg>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/05 23:30:41 by amtan             #+#    #+#             */
-/*   Updated: 2026/03/06 11:46:41 by amtan            ###   ########.fr       */
+/*   Updated: 2026/03/06 13:09:00 by amtan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,12 +67,24 @@ static void	ms_exec_cmd(t_info *i, t_ast *ast)
 	ft_sfree((void **)&path);
 }
 
-void	ms_exec_ast(t_info *i, t_ast *ast)
+static void	ms_exec_node(t_info *i, t_ast *ast)
 {
 	if (!i || !ast)
 		return ;
-	if (ms_heredoc_prepare_ast(i, ast))
+	if (ast->type == AST_AND)
+	{
+		ms_exec_node(i, ast->left);
+		if (i->err == 0)
+			ms_exec_node(i, ast->riht);
 		return ;
+	}
+	if (ast->type == AST_OR)
+	{
+		ms_exec_node(i, ast->left);
+		if (i->err != 0)
+			ms_exec_node(i, ast->riht);
+		return ;
+	}
 	if (ast->type == AST_PIPE)
 	{
 		i->err = ms_exec_pipe(i, ast);
@@ -81,4 +93,13 @@ void	ms_exec_ast(t_info *i, t_ast *ast)
 	if (ast->type != AST_CMD || !ast->args || !ast->args[0])
 		return ;
 	ms_exec_cmd(i, ast);
+}
+
+void	ms_exec_ast(t_info *i, t_ast *ast)
+{
+	if (!i || !ast)
+		return ;
+	if (ms_heredoc_prepare_ast(i, ast))
+		return ;
+	ms_exec_node(i, ast);
 }
